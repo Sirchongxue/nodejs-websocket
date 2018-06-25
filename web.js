85f96cbfaf9b76a4807da643c390280689e436f1
chongxue <1804082987@qq.com>
2018-06-25 16:49:52 +0800

    first commit

--- web.js
+++ web.js
@@ -1,7 +1,50 @@
-var ws=require('nodejs-websocket');
-var SerialPort = require('serialport')
+var express = require('express');  
+var app = express();  
+var expressWs = require('express-ws')(app);  
+var util = require('util');  
+app.use(express.static('public'));  
+//ejs
+app.set('view engine','ejs');
 
-var server = ws.createServer(function (conn, res) {
+app.get('/',function(req,res){
+    res.render('control');
+})
+
+app.ws('/ws', function(ws, req) {  
+  util.inspect(ws);  
+ 
+    ws.on("message",function(str){
+        hander(str,ws);
+         
+    });
+    ws.on("close",function(code,reason){
+        console.log('connection closed');
+    })
+    //处理错误事件信息
+    ws.on('error',function(err){
+        console.log('throw err',err);
+    })
+
+}) 
+function hander(str,ws) {  
+    //发送数据到客户端
+    //console.log(str);
+    var arr = str.split('');
+    if(arr[2]==1){//close
+        arr[2]=0;
+    }else{
+        arr[2]=1;
+    }
+    //console.log(arr.join(''));
+    ws.send(arr.join(''));   
+}
+
+app.listen(3000);
+ 
+
+
+
+/*var server = ws.createServer(function (conn, res) {
     conn.on("text",function(str){
         //broadcast(server,str.split(','));
         //console.log(str);
@@ -31,3 +74,4 @@ function hander(str,conn) {
     //console.log(arr.join(''));
       conn.sendText(arr.join(''));   
 }
+*/
